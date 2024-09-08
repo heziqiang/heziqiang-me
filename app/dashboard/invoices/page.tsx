cbd7a35048beee6543e327bf125e4a6586c99abf
Ziqiang He <ziqianghe123@gmail.com>
2024-09-08 22:05:42 +0800

    feat: streaming

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,4 +1,8 @@
-export default function Invoices() {
+export default async function Invoices() {
+  await new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('success');
+    }, 2000);
+  });
   return <p>Invoices Page</p>;
-};
-
+}
